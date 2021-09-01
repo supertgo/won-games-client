7e1f274e1b0131fa167f7411cd60cbf719584d1b
supertgo <thiagoroberto2003@gmail.com>
2021-09-01 13:47:16 -0300

    Added full width option

---
+++ src/components/Button/test.tsx
@@ -0,0 +1,44 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import Button from '.';
+
+describe('<Button />', () => {
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>Buy Now</Button>);
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    });
+  });
+
+  it('should render the small size', () => {
+    renderWithTheme(<Button size="small">Buy Now</Button>);
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '3rem',
+      padding: '0.8rem',
+      'font-size': '1.2rem'
+    });
+  });
+
+  it('should render the large size', () => {
+    renderWithTheme(<Button size="large">Buy Now</Button>);
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.8rem',
+      'font-size': '1.6rem'
+    });
+  });
+
+  it('should render fullWidth version', () => {
+    renderWithTheme(<Button fullWidth>Buy Now</Button>);
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      width: '100%'
+    });
+  });
+});
