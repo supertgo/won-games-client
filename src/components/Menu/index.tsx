561e32ace13393bf81505cea7106e2b6bf623d76
supertgo <thiagoroberto2003@gmail.com>
2021-09-05 08:55:29 -0300

    Creating styles for the MenuFull Component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react';
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2';
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart';
@@ -5,8 +6,8 @@ import { Search as SearchIcon } from '@styled-icons/material-outlined/Search';
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 
 import Logo from 'components/Logo';
+import Button from 'components/Button';
 import * as S from './styles';
-import React from 'react';
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,6 +30,18 @@ const Menu = () => {
       </S.MenuGroup>
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+
+        <S.RegisterBox>
+          <Button fullWidth>Log in now</Button>
+          <span>ou</span>
+          <S.CreateAccount href="#" title="Sign Up">
+            Sign Up
+          </S.CreateAccount>
+        </S.RegisterBox>
       </S.MenuFull>
     </S.Wrapper>
   );
