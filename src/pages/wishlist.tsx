77e2313ace126e25cb8f0a1299f5b306f90fbded
supertgo <thiagoroberto2003@gmail.com>
2021-10-03 17:58:27 -0300

    Added new route to the project

---
+++ src/pages/wishlist.tsx
@@ -0,0 +1,18 @@
+import Wishlist from 'templates/Wishlist';
+import highlightMock from 'components/Highlight/mock';
+import gamesMock from 'components/GameCardSlider/mock';
+
+import { WishlistTemplateProps } from 'templates/Wishlist';
+
+export default function WishlistPage(props: WishlistTemplateProps) {
+  return <Wishlist {...props} />;
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      recommendedGames: gamesMock.slice(0, 5),
+      recommendedHighlight: highlightMock
+    }
+  };
+}
