b47e1ff86bfb7845387ee15097c998779cf0fbe2
supertgo <thiagoroberto2003@gmail.com>
2021-10-07 15:59:31 -0300

    Finishing PaymentOption Component

--- src/components/PaymentOptions/stories.tsx
+++ src/components/PaymentOptions/stories.tsx
@@ -13,11 +13,22 @@ export default {
     backgrounds: {
       default: 'won-dark'
     }
+  },
+  argTypes: {
+    handlePayment: {
+      action: 'clicked'
+    }
   }
 } as Meta;
 
 export const Default: Story<PaymentOptionsProps> = (args) => (
   <div style={{ padding: 16, maxWidth: 400 }}>
     <PaymentOptions {...args} />
+  </div>
+);
+
+export const WithNoCards: Story<PaymentOptionsProps> = () => (
+  <div style={{ padding: 16, maxWidth: 400 }}>
+    <PaymentOptions handlePayment={() => ({})} />
   </div>
 );
