fb19e920c4e07b0627f3524a71a44c028c0a7508
supertgo <thiagoroberto2003@gmail.com>
2021-10-12 14:19:44 -0300

    Utilizing Apollo Codegen to generate queries

---
+++ src/utils/apollo.ts
@@ -0,0 +1,36 @@
+import { useMemo } from 'react';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from '@apollo/client';
+
+let apolloClient: ApolloClient<NormalizedCacheObject>;
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined', //verificar se é ssr
+    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    cache: new InMemoryCache()
+  });
+}
+
+export function initializeApollo(initialState = {}) {
+  const apolloClientGlobal = apolloClient ?? createApolloClient();
+
+  if (initialState) {
+    apolloClientGlobal.cache.restore(initialState);
+  }
+
+  if (typeof window === 'undefined') return apolloClientGlobal;
+  apolloClient = apolloClient ?? apolloClientGlobal;
+
+  return apolloClient;
+}
+
+export function useApollo(initialState = {}) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+
+  return store;
+}
