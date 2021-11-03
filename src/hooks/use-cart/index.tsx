fdea53aa6ff9525a509b0005d47236bc1d8c94d3
supertgo <thiagoroberto2003@gmail.com>
2021-11-03 14:06:15 -0300

    Creating hook test with testing library hooks

---
+++ src/hooks/use-cart/index.tsx
@@ -0,0 +1,40 @@
+import { useContext, createContext, useState, useEffect } from 'react';
+import { getStorageItem } from 'utils/localStorage';
+
+const CART_KEY = 'cartItems';
+
+export type CartContextData = {
+  items: string[];
+};
+
+export const CartContextDefaultValue = {
+  items: []
+};
+
+export const CartContext = createContext<CartContextData>(
+  CartContextDefaultValue
+);
+
+export type CartProviderProps = {
+  children: React.ReactNode;
+};
+
+const CartProvider = ({ children }: CartProviderProps) => {
+  const [cartItems, setCartItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    const data = getStorageItem(CART_KEY);
+
+    if (data) setCartItems(data);
+  }, []);
+
+  return (
+    <CartContext.Provider value={{ items: cartItems }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+const useCart = () => useContext(CartContext);
+
+export { CartProvider, useCart };
