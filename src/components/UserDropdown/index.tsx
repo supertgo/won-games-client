3ba08a72282283b6bc349abca0667172d2a89b7a
supertgo <thiagoroberto2003@gmail.com>
2022-03-26 09:49:31 -0300

    fix(pages): :bug: redirect whem sign out

--- src/components/UserDropdown/index.tsx
+++ src/components/UserDropdown/index.tsx
@@ -8,6 +8,7 @@ import {
 import { ChevronDown } from '@styled-icons/boxicons-regular/ChevronDown';
 
 import Dropdown from 'components/Dropdown';
+import { useRouter } from 'next/router';
 
 import * as S from './styles';
 
@@ -15,36 +16,46 @@ export type UserDropdownProps = {
   username: string;
 };
 
-const UserDropdown = ({ username }: UserDropdownProps) => (
-  <Dropdown
-    title={
-      <>
-        <AccountCircle size={24} />
-        <S.Username>{username}</S.Username>
-        <ChevronDown size={24} />
-      </>
-    }
-  >
-    <S.Nav>
-      <Link href="/profile/me" passHref>
-        <S.Link>
-          <AccountCircle />
-          <span>My profile</span>
-        </S.Link>
-      </Link>
-      <Link href="/wishlist" passHref>
-        <S.Link title="Wishlist">
-          <FavoriteBorder />
-          <span>Wishlist</span>
-        </S.Link>
-      </Link>
+const UserDropdown = ({ username }: UserDropdownProps) => {
+  const { push } = useRouter();
+  return (
+    <Dropdown
+      title={
+        <>
+          <AccountCircle size={24} />
+          <S.Username>{username}</S.Username>
+          <ChevronDown size={24} />
+        </>
+      }
+    >
+      <S.Nav>
+        <Link href="/profile/me" passHref>
+          <S.Link>
+            <AccountCircle />
+            <span>My profile</span>
+          </S.Link>
+        </Link>
+        <Link href="/wishlist" passHref>
+          <S.Link title="Wishlist">
+            <FavoriteBorder />
+            <span>Wishlist</span>
+          </S.Link>
+        </Link>
 
-      <S.Link role="button" onClick={() => signOut()} title="Sign out">
-        <ExitToApp />
-        <span>Sign out</span>
-      </S.Link>
-    </S.Nav>
-  </Dropdown>
-);
+        <S.Link
+          role="button"
+          title="Sign out"
+          onClick={async () => {
+            const data = await signOut({ redirect: false, callbackUrl: '/' });
+            push(data.url);
+          }}
+        >
+          <ExitToApp />
+          <span>Sign out</span>
+        </S.Link>
+      </S.Nav>
+    </Dropdown>
+  );
+};
 
 export default UserDropdown;
