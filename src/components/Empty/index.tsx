15bedf062a22c619b34c424f8a0f259c8d41cfe1
supertgo <thiagoroberto2003@gmail.com>
2022-03-28 09:24:21 -0300

    feat(components): :sparkles: add next image to Empty component

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -2,6 +2,8 @@ import Button from 'components/Button';
 import Link from 'next/link';
 import * as S from './styles';
 
+import Image from 'next/image';
+
 export type EmptyProps = {
   title: string;
   description: string;
@@ -10,10 +12,11 @@ export type EmptyProps = {
 
 const Empty = ({ title, description, hasLink }: EmptyProps) => (
   <S.Wrapper>
-    <S.Image
+    <Image
       src="/img/empty.svg"
       alt="A gamer in a couch playing videogame"
-      role="image"
+      width={380}
+      height={285}
     />
 
     <S.Title>{title}</S.Title>
