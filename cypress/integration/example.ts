c305a79eaf1fd582f0197348dcce96c5b5067fa0
supertgo <thiagoroberto2003@gmail.com>
2022-03-31 09:37:59 -0300

    feat(cypress): :sparkles: add cypress and ts config to it

---
+++ cypress/integration/example.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../support/index.d.ts" />
+
+describe('Cypress ts', () => {
+  it('should go to google', () => {
+    cy.google();
+  });
+});
