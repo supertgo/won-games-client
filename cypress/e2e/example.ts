d163212fcac0e98d136bd6e8837440e2bc97726f
supertgo <thiagoroberto2003@gmail.com>
2022-04-03 07:25:29 -0300

    feat(cypress): :sparkles: add command to start next and cypress

--- cypress/e2e/example.ts
+++ cypress/e2e/example.ts
@@ -1,10 +1,11 @@
 /// <reference path="../support/index.d.ts" />
 
-describe('Cypress ts', () => {
-  it.skip('should go to google', () => {
+describe.skip('Cypress ts', () => {
+  it('should go to google', () => {
     cy.google();
   })
-  it.skip('should change light/dark theme on willian justen site', () => {
+
+  it('should change light/dark theme on willian justen site', () => {
     cy.visit('https://willianjusten.com.br')
     cy.findByTitle(/mudar o tema/i).click()
     cy.get('.light').should('exist')
