5edf8e414d5f9b1c770c9e0cd2b442f0261f87f7
supertgo <thiagoroberto2003@gmail.com>
2022-04-06 09:48:14 -0300

    feat(cypress): :sparkles: downgrade cypress version

---
+++ cypress/e2e/game.ts
@@ -0,0 +1,12 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Game Page', () => {
+  it('should render game page sections', () => {
+    cy.visit('/game/cyberpunk-2077')
+
+    cy.getByDataCy("game-info").within(() => {
+      cy.findByRole('heading', {name: /Cyberpunk 2077/i}).should('exist')
+
+    })
+  })
+})
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -28,7 +28,8 @@
 import '@testing-library/cypress/add-commands';
 
 
-Cypress.Commands.add('google', () => cy.visit('https://google.com'));
+Cypress.Commands.add('google', () => cy.visit('https://google.com'))
+
 
 Cypress.Commands.add('getByDataCy', (selector, ...args) => {
   return cy.get(`[data-cy="${selector}"]`, ...args)
@@ -37,26 +38,28 @@ Cypress.Commands.add('getByDataCy', (selector, ...args) => {
 Cypress.Commands.add('shouldRenderBanner', () => {
   cy.get('.slick-slider').within(() => {
     cy.findByRole('heading', { name: 'The Witcher 3: Wild Hunt' })
+ 
     cy.findByRole('link', { name: /buy now/i })
-    
+
     cy.get('.slick-dots > :nth-child(2) > button').click()
     cy.wait(500)
-    cy.findByRole('heading', { name: 'Cyberpunk 2077'})
+
+    cy.findByRole('heading', { name: /cyberpunk 2077/i })
+    cy.findByRole('link', { name: /buy now/i })
   })
-});
+})
 
-
-Cypress.Commands.add('shouldRenderShowcase', ({ name, highlight = false }) => {
+Cypress.Commands.add('shouldRenderShowcase', ({ name,  highlight = false }) => {
   cy.getByDataCy(name).within(() => {
     cy.findByRole('heading', { name }).should('exist')
 
-    cy.getByDataCy("highlight").should(highlight ? 'exist' : 'not.exist')
+    cy.getByDataCy('highlight').should(highlight ? 'exist' : 'not.exist')
 
     if (highlight) {
-      cy.getByDataCy("highlight").within(() => {
+      cy.getByDataCy('highlight').within(() => {
         cy.findByRole('link').should('have.attr', 'href')
       })
     }
   })
-  cy.getByDataCy("game-card").should('have.length.gt', 0)
+  cy.get(`[data-cy="game-card"]`).should('have.length.gt', 0)
 })
